6e72bca4065d70b1294e1672583875dbed9fd956
535353 <535353@DESKTOP-LLEE8VM>
2023-10-19 11:20:48 +0900

    메인보드 디에이오 작업마무리 커멘트는 이 아래로 써주세요 :D

--- src/main/java/app/dao/MainBoardDao.java
+++ src/main/java/app/dao/MainBoardDao.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import app.dbconn.DbConn;
-import app.domain.BoardVo;
 import app.domain.MainBoardVo;
+import app.domain.MemberVo;
 import app.domain.SearchCriteria;
 
 public class MainBoardDao {
@@ -21,6 +21,113 @@ public class MainBoardDao {
 		this.conn = dbconn.getConnection();
 	}
 	
+	public int BoardInsert(MainBoardVo mbv) {
+		
+		int exec = 0;
+		
+		String sql = "INSERT INTO mainboard0803(boardtype,SUBJECT,CONTENTS,midx,filename)\r\n"
+				+ "VALUES(?,?,?,?,?)";
+
+		
+		try{
+		conn.setAutoCommit(false);
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, mbv.getBoardtype());
+		pstmt.setString(2, mbv.getSubject());
+		pstmt.setString(3, mbv.getContents());
+		pstmt.setInt(4, mbv.getMidx());
+		pstmt.setString(5, mbv.getFilename());
+		pstmt.executeUpdate();
+
+		
+		conn.commit();
+		
+		}catch(Exception e){
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
+		return exec;	
+	}
+	
+	public int boardTotalCount(SearchCriteria scri){
+		int value=0;  // 결과값이 0인지 아닌지
+		
+		String str="";
+		if(!scri.getKeyword().equals("")) {
+			str=" and "+scri.getSearchType()+" like concat('%','"+scri.getKeyword()+"','%')";
+		}
+		
+		String sql="select count(*) as cnt from mainboard0803 where delyn='N'"+str;
+		ResultSet rs = null;
+		try{
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()){
+				value =	rs.getInt("cnt");
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				rs.close();
+				pstmt.close();
+				conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		return value;
+	}
+	
+	public MainBoardVo boardSelectOne(int bidx) {
+		MainBoardVo mbv = null;
+		MemberVo mv = null;
+		String sql="select * from mainboard0803 a join memberb on a.midx=b.midx where bidx=?";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		//향상된 구문 클래스스를 꺼낸다
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, bidx);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				//bv생성하고 결과값 옮겨담기
+				mbv = new MainBoardVo();
+				mv = new MemberVo();
+				mbv.setSubject(rs.getString("subject"));
+				mbv.setContents(rs.getString("contents"));
+				mv.setMemberId(rs.getString("memberId"));
+				mv.setNickname(rs.getString("nickname"));
+				mbv.setBidx(rs.getInt("bidx"));
+				mbv.setViewcnt(rs.getInt("viewcnt"));
+				
+			}
+			
+		} catch (SQLException e) {			
+			e.printStackTrace();
+		}finally{
+			try{
+				rs.close();
+				pstmt.close();
+				conn.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		return mbv;
+	}
+	
 	public ArrayList<MainBoardVo>  boardSelectAll(SearchCriteria scri){
 		//무한배열클래스 객체생성해서 데이터를 담을 준비를 한다
 		ArrayList<MainBoardVo> alist =new ArrayList<MainBoardVo>();
@@ -31,11 +138,11 @@ public class MainBoardDao {
 		if(!scri.getKeyword().equals("")) {
 			str=" and "+scri.getSearchType()+" like concat('%','"+scri.getKeyword()+"','%')";
 		}
-		String sql="select boardtype,bidx,subject,viewcnt,writeday\r\n"
-				+ "from mainboard \r\n"
+		String sql="select a.boardtype,a.bidx,a.subject,a.viewcnt,a.writeday,b.memberid,b.nickname\r\n"
+				+ "from mainboard a join member b on a.midx=b.midx \r\n"
 				+ "where delyn='N'\r\n"
 				+ str
-				+ "order by originbidx DESC,depth limit ?,?";
+				+ "order by bidx DESC limit ?,?";
 		try{
 			//1.창고(컬렉션)를 만든다
 			//2.쿼리를 실행해서 데이터를 전용객체에 담아온다 
@@ -51,14 +158,17 @@ public class MainBoardDao {
 			rs = pstmt.executeQuery();
 			//rs.next()는 다음값이 있는지 확인하는 메소드 있으면true
 			while(rs.next()){
-				MainBoardVo bv = new MainBoardVo();
+				MainBoardVo mbv = new MainBoardVo();
+				MemberVo mv = new MemberVo();
 				//rs에서 midx값 꺼내서 mv에 옮겨담는다
-				bv.setBoardtype(rs.getString("boardtype"));
-				bv.setBidx( rs.getInt("Bidx") ); 
-				bv.setSubject( rs.getString("Subject") );
-				bv.setViewcnt( rs.getInt("viewcnt"));
-				bv.setWriteday( rs.getString("writeday"));
-				alist.add(bv);
+				mbv.setBoardtype(rs.getString("boardtype"));
+				mbv.setBidx( rs.getInt("Bidx") ); 
+				mbv.setSubject( rs.getString("Subject") );
+				mbv.setViewcnt( rs.getInt("viewcnt"));
+				mbv.setWriteday( rs.getString("writeday"));
+				mv.setMemberId(rs.getString("memberId"));
+				mv.setNickname(rs.getString("nickname"));
+				alist.add(mbv);
 			}		
 			
 		}catch(Exception e){
@@ -76,40 +186,70 @@ public class MainBoardDao {
 		return alist;
 	}
 	
-	public int BoardInsert(MainBoardVo bv) {
+	public int BoardCntUpdate(int bidx) {
 		
 		int exec = 0;
 		
-		String sql = "INSERT INTO mainboard0803(boardtype,SUBJECT,CONTENTS,WRITER,midx,pwd,filename)\r\n"
-				+ "VALUES(Null,0,0,?,?,?,?,?,?)";
-		String sql2 = "UPDATE board0803 set \r\n"
-				+ "originbidx =(SELECT a.bidx from(select max(bidx) as bidx from board0803 )a)\r\n"
-				+ "where bidx=(SELECT a.bidx from(select max(bidx) as bidx from board0803 )a)";
+		String sql = "UPDATE mainboard0803 set \r\n"
+				+ "viewcnt =viewcnt+1\r\n"
+				+ "where bidx=?";
 		
 		try{
-		conn.setAutoCommit(false);
 		pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, bv.getSubject());
-		pstmt.setString(2, bv.getContents());
-		pstmt.setString(3, bv.getWriter());
-		pstmt.setInt(4, bv.getMidx());
-		pstmt.setString(5, bv.getPwd());
-		pstmt.setString(6, bv.getFilename());
-		pstmt.executeUpdate();
-		
-		pstmt = conn.prepareStatement(sql2);
+		pstmt.setInt(1,bidx);
 		exec = pstmt.executeUpdate();
-		
-		conn.commit();
-		
+
 		}catch(Exception e){
-			try {
-				conn.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
 			e.printStackTrace();
-		}
-		return exec;	
+		}	
+			return exec;		
 	}
+	
+public int BoardModify(MainBoardVo bv) {
+		
+		int exec = 0;
+		
+		
+		String sql = "UPDATE mainboard0803 set \r\n"
+				+ "subject =?, \r\n"
+				+ "contents =?, \r\n"
+				+ "modifyday = now(), \r\n"
+				+ "ip =? \r\n"
+				+ "where bidx = ?";
+		
+		try{
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1,bv.getSubject());
+		pstmt.setString(2,bv.getContents());
+		pstmt.setString(3,bv.getIp());
+		pstmt.setInt(4,bv.getBidx());
+		
+		exec = pstmt.executeUpdate();
+		//수정이 되면 1이 리턴
+		}catch(Exception e){
+			e.printStackTrace();
+		}	
+			return exec;			
+	}
+	
+	public int boardDelete(int bidx) {
+	
+	int exec = 0;
+	
+	
+	String sql ="UPDATE mainboard0803 set \r\n"
+			+ "delyn='Y', modifyday=now() \r\n"
+			+ "where bidx=?";
+	
+	try {
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setInt(1,bidx);
+		exec = pstmt.executeUpdate();
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	
+	return exec;
+}
+	
 }
